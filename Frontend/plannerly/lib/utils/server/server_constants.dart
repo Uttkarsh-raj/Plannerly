4a72553c2cd4bc61d2a6682ba9580a5967f98afb
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2023-11-07 20:03:46 +0530

    backend is live

--- Frontend/plannerly/lib/utils/server/server_constants.dart
+++ Frontend/plannerly/lib/utils/server/server_constants.dart
@@ -1,5 +1,6 @@
 //just for testing
 var userId = "653a2f71e2330ac369e93c9b";
-var baseUrl = "http://192.168.2.52:8000";
+// var baseUrl = "http://192.168.2.52:8000";
+var baseUrl = "https://go-plannerly-api.onrender.com";
 var token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6ImpvaG4uZG9lQGdtYWlsLmNvbSIsIkZpcnN0X25hbWUiOiJKb2huIiwiTGFzdF9uYW1lIjoiRG9lIiwiVWlkIjoiNjUzYTJmNzFlMjMzMGFjMzY5ZTkzYzliIiwiZXhwIjoxNjk5Mjc3MTYwfQ.xvzZ3bdzAagcorXju4cR8o3M51ZC0ZILbMxQmnYG4DE";
